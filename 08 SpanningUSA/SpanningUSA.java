eadc38930b3256bba05dcd20492dd402427569f3
Simon  Flachs <Simon@Simons-MacBook-Air-4.local>
2016-04-26 14:57:12 +0200

    working solution, i think

--- 08 SpanningUSA/SpanningUSA.java
+++ 08 SpanningUSA/SpanningUSA.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.*; 
 import java.util.Hashtable;
 import java.util.Comparator;
+import java.util.Random;
 
 public class SpanningUSA {
 	
@@ -9,8 +10,7 @@ public class SpanningUSA {
 	public static void main(String[] args) {
 		String[] lines = new In(args[0]).readAll().split("\\n");
 		Hashtable map = new Hashtable();
-		//Source: http://algs4.cs.princeton.edu/43mst/EdgeWeightedGraph.java.html
-		EdgeWeightedGraph g = new EdgeWeightedGraph(cities);
+		EdgeWeightedGraph g = new EdgeWeightedGraph(cities); //Source: http://algs4.cs.princeton.edu/43mst/EdgeWeightedGraph.java.html
 
 		/*
 		 * 	Collect all cities in an array
@@ -18,24 +18,8 @@ public class SpanningUSA {
 		for (int i = 0; i < cities; i++) {
 			lines[i] = lines[i].replaceAll("\"", "").trim();
 			map.put(lines[i], new Integer(i));
-			//StdOut.println(lines[i]);
 		}
 		
-
-		MinPQ pq = new MinPQ(new Comparator(){
-			public int compare(Object o1, Object o2) {
-				Edge e1 = (Edge) o1;
-				Edge e2 = (Edge) o2;
-				if (e1.weight() < e2.weight()) {
-					return -1;
-				} else if (e1.weight() == e2.weight()) {
-					return 0;
-				} else {
-					return 1;
-				}
-			}
-		});
-
 		/*
 		 * 	Collect all edges between cities
 		 */
@@ -55,45 +39,67 @@ public class SpanningUSA {
 			String to = second[0].trim();
 
 			int weg = Integer.parseInt(second[1].substring(0, second[1].length()-1));
-			//StdOut.println("from - to - weg: "+from +to +weg);
-			//StdOut.println("from: "+from);
-			//StdOut.println("to: "+to);
-
-			Edge edge = new Edge((Integer) map.get(from), (Integer) map.get(to), weg);
-			//StdOut.println((Integer) map.get(to));
-
 			
+			Edge edge = new Edge((Integer) map.get(from), (Integer) map.get(to), weg);
+				
 			g.addEdge(edge);
-			//pq.insert(edge);
+			
 		}
 
-		// pick random vertice as a starting point for the tree
-		int start = 10;
-
-		for (Edge edge: g.adj(start))
-		{
-			pq.insert(edge);
-		}
-
-		Edge edge = (Edge) pq.min();
-		StdOut.println(edge.weight());
-
-		StdOut.println(edge.other(start));
-
-		// pick min edge
-		// add new vertice to tree
-		// add new edges to priority queue
-
+		prims(g, cities);
 		
-
-		//prims(g);
-
-		//Source: http://algs4.cs.princeton.edu/43mst/KruskalMST.java.html
-		//KruskalMST mst = new KruskalMST(g);
-		//StdOut.println("Weight: " + mst.weight());
 	}
 
-	private static void prims(EdgeWeightedGraph graph){
+	private static void prims(EdgeWeightedGraph g, int cities){
 
+
+		MinPQ pq = new MinPQ(new Comparator(){
+			public int compare(Object o1, Object o2) {
+				Edge e1 = (Edge) o1;
+				Edge e2 = (Edge) o2;
+				if (e1.weight() < e2.weight()) {
+					return -1;
+				} else if (e1.weight() == e2.weight()) {
+					return 0;
+				} else {
+					return 1;
+				}
+			}
+		});
+
+		Queue<Edge> mst = new Queue<Edge>();
+
+		// pick random vertice as a starting point for the tree
+		Random ran = new Random();
+		int newestVertice = ran.nextInt(cities);
+		
+		int treeSize = 1;
+		Boolean[] marked = new Boolean[cities];
+		marked[newestVertice] = true;
+
+		while(treeSize <= cities)
+		{	
+			for (Edge edge: g.adj(newestVertice))
+			{
+				int other = edge.other(newestVertice);
+				
+				// only insert edge if adjecent vertix is outside tree
+				if(marked[other] == null) pq.insert(edge);
+			}
+
+			Edge edge = (Edge) pq.delMin();
+
+			// get the vertex outside the tree
+			int either = edge.either();
+			int other = edge.other(either);
+			newestVertice = marked[either] == null ? either : other;
+
+			marked[newestVertice] = true;
+			mst.enqueue(edge);
+			treeSize ++;
+		}
+
+	
+		StdOut.println(mst.toString());
 	}
 }
