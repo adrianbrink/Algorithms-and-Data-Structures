619309760601064f599c32a8dcf858fcf9294e4d
Asger <holyxhellx@users.noreply.github.com>
2016-04-29 15:25:05 +0200

    GorillaHash - mostly complete (#34)
    The print method is borrowed and does not print all statements fully.

--- 06 GorillaHash/GorillaHash.java
+++ 06 GorillaHash/GorillaHash.java
@@ -4,13 +4,73 @@ import java.util.Arrays;
 
 public class GorillaHash {
 
+    /*
+     *  Calculate the similarities between two input arrays
+     *  Source: http://introcs.cs.princeton.edu/java/34nbody/Vector.java.html
+     */
+    private static double similar(int[] p, int[] q) {
+        
+        
+        double similar, dot = 0.0;
+        
+        //Collect the inner product of this Vector p and q
+        //dot = p dot q = p1 * q1 + p2 * q2 .... and so on
+        for (int i = 0; i < p.length; i++) dot = dot + (p[i] * q[i]);
+   
+        //Collect the lenght of this Vector p and q (Euclidean dist.)
+        //pytagoras: a^2 + b^ = c^2
+        similar = dot / (dist(p) * dist(q));
+        
+        return similar;
+    }
+    
+    /*
+     *  Returns the length of a vector
+     *  Source: http://introcs.cs.princeton.edu/java/34nbody/Vector.java.html
+     */
+    private static double dist(int[] a) {
+        double aLength = 0;
+        
+        for (int i = 0 ; i < a.length ; i++) aLength = aLength + Math.pow(a[i], 2);
+        return Math.sqrt(aLength);
+    }
+
+    /*
+     *  Returns the hashed profile 
+     */ 
+    private static int[] profile(String s, int gramLength, int profileLength) {
+        //Setup profile size, set by profilLength
+        int[] profile = new int[profileLength];
+        
+        //Setup gram size, set by gramLength related to s(dnaline)
+        String[] gram = grams(gramLength, s);
+
+        //Hash the remainder of profileLength of gram to the profile
+        for (int i = 0; i < gram.length ; i++) {
+            int hash = Math.abs(gram[i].hashCode()) % profileLength;
+            profile[hash] = profile[hash] +1;
+        }
+        return profile;
+    } 
+
+    private static String[] grams(int gramLength, String s) {
+
+        String[] gram = new String[s.length()-gramLength]; 
+        for (int i = 0; i < gram.length; i++) {
+            int end = i + gramLength;
+            gram[i] = s.substring(i, end);
+        }
+        return gram;
+    }
+
+
 	public static void main(String[] args) {
 
         
         //The gram length - Initial value k = 20 
         int k = 20;
 
-        //The profil length - Initial value d = 10000
+        //The profil length used in a reminder calc. in Method: "profile" - Initial value d = 10000
         int d = 10000;
 
         //Create a string of array for each line in the file.   
@@ -29,52 +89,43 @@ public class GorillaHash {
         for(int i = 0 ; i < lines.length ; i++) {
             if (lines[i].contains(">")) {
                 numberOfSpecies++;
-                species[numberOfSpecies] = lines[i].replace(">", "");
+                species[numberOfSpecies] = lines[i].replace(">", "").trim().replaceAll("\\d+.*", "");
             } else  dnaLine[numberOfSpecies] = dnaLine[numberOfSpecies] + lines[i];
         }
         
         //Create profiles in an two dimentional array.... Fuck this shit!!! (Do the Pong Dance)
-        int [][] profil = new int[numberOfSpecies][d];
+        int [][] profile = new int[numberOfSpecies][d];
         
-        for(int i = 0 ; i < numberOfSpecies ; i++) profil[i] = getProfil(dnaLine[i], k, d);
+        for(int i = 0 ; i < numberOfSpecies ; i++) profile[i] = profile(dnaLine[i], k, d);
         
-        //Output - test
-        for(int i = 0 ; i <= numberOfSpecies; i++) {
-            StdOut.println(i + ": " + species[i]);
-            StdOut.println(i + ": " + dnaLine[i]);
+        //Output - printstatement:
+            for(int i = 0; i < dnaLine.length; i++) {
+                StdOut.println(i+species[i]);
+                StdOut.println(i+dnaLine[i]);
+            }
+            
+        //print the overview-top-bar with species (names)
+        System.out.printf("%15s", "");
+        for (int i = 0; i < species.length; i++) {
+            System.out.printf("%15s", species[i]);
         }
+        //Next line
+        StdOut.println("");
+        
+        //print names first in the row (i) and profiles compared (j)
+        for (int i = 0; i < profile.length; i++) {
+            System.out.printf("%15s", species[i]);
+            for (int j = 0; j < profile.length; j++) {
+                double similar = similar(profile[i], profile[j]);
+                System.out.printf("%15f", similar);
+            }
+            System.out.println();
+        }
+        
+        //Sea-Cucumber - print
+        System.out.printf("%15s", species[11]);
+        System.out.printf("%15f", similar(profile[11], profile[0]));
+        StdOut.println("");
+        StdOut.println(profile.length);
     }
-    
-    //Calculate the similarities between the profiles based on hashing
-    private static double similar(int[] p, int[] q) {
-        //Helpers: http://introcs.cs.princeton.edu/java/34nbody/Vector.java.html
-        
-        double similar, dot = 0.0;
-        
-        //Collect the inner product of this Vector p and q
-        //dot = p dot q = p1 * q1 + p2 * q2 .... and so on
-        for (int i = 0; i < p.length; i++) dot = dot + (p[i] * q[i]);
-   
-        //Collect the lenght of this Vector p and q (Euclidean dist.)
-        //pytagoras: a^2 + b^ = c^2
-        double pLength = dist(p);
-        double qLength = dist(q);
-        double totalLength = pLength * qLength;
-        
-        similar = dot / totalLength;
-        
-        return similar;
-    }
-    
-    private static double dist(int[] a) {
-        double aLength = 0;
-        
-        for (int i = 0 ; i < a.length ; i++) aLength = aLength + Math.pow(a[i], 2);
-        return Math.sqrt(aLength);
-    }
-    
-    private static int[] getProfil(String s, int gramLength, int profilLength) {
-        int[] profil = new int[profilLength];
-        return profil;
-    } 
 }
